a88786125d6a8d3c86777d3983f93f2cb76ff546
omnivascular <134437164+omnivascular@users.noreply.github.com>
2024-01-08 10:24:28 -0500

    Update myApp.js
    Adding all steps up until prior Replit position for tutorial, using createManyPeople method.

--- myApp.js
+++ myApp.js
@@ -1,15 +1,56 @@
-require('dotenv').config();
+require("dotenv").config();
+let mongoose = require("mongoose");
 
+const mongoURI = process.env.MONGO_URI;
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: Number,
+  favoriteFoods: [String],
+});
 
-const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+let Person = mongoose.model("Person", personSchema);
+
+const createAndSavePerson = function (done) {
+  var person_1 = new Person({
+    name: "John Doe",
+    age: 33,
+    favoriteFoods: ["pizza", "burgers"],
+  });
+  person_1.save(function (err, data) {
+    if (err) {
+      return console.error(err);
+    }
+    console.log("saved person object");
+    done(null, data);
+  });
 };
 
-const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+const arrayOfPeople = [{ name: "Joe" }, { name: "Ellis" }, { name: "Jack" }];
+
+const createManyPeople = function (arrayOfPeople, done) {
+  Person.create(arrayOfPeople, function (err, people) {
+    if (err) {
+      console.error(err); // Log the error
+      return done(err); // Pass the error to the callback
+    }
+    console.log(people);
+    done(null, people);
+  });
 };
+
+// Usage example
+createManyPeople(arrayOfPeople, (err, result) => {
+  if (err) {
+    console.error("Error creating people:", err);
+  } else {
+    console.log("People created successfully:", result);
+  }
+});
 
 const findPeopleByName = (personName, done) => {
   done(null /*, data*/);
